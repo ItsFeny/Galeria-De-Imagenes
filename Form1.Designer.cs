417ce6c13a18969f899a36560c796ec88080eed8
ItsFeny <fenymartinez459@gmail.com>
2022-06-01 11:08:17 -0400

    v1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace Galeria_De_Imagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.p6 = new System.Windows.Forms.PictureBox();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Animes:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Peru;
+            this.groupBox1.Controls.Add(this.p6);
+            this.groupBox1.Controls.Add(this.p5);
+            this.groupBox1.Controls.Add(this.p4);
+            this.groupBox1.Controls.Add(this.p3);
+            this.groupBox1.Controls.Add(this.p2);
+            this.groupBox1.Controls.Add(this.p1);
+            this.groupBox1.Location = new System.Drawing.Point(289, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(469, 597);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // p6
+            // 
+            this.p6.Image = global::Galeria_De_Imagenes.Properties.Resources.Signo;
+            this.p6.Location = new System.Drawing.Point(251, 406);
+            this.p6.Name = "p6";
+            this.p6.Size = new System.Drawing.Size(200, 173);
+            this.p6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p6.TabIndex = 8;
+            this.p6.TabStop = false;
+            // 
+            // p5
+            // 
+            this.p5.Image = global::Galeria_De_Imagenes.Properties.Resources.Signo;
+            this.p5.Location = new System.Drawing.Point(19, 406);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(200, 173);
+            this.p5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p5.TabIndex = 7;
+            this.p5.TabStop = false;
+            // 
+            // p4
+            // 
+            this.p4.Image = global::Galeria_De_Imagenes.Properties.Resources.Signo;
+            this.p4.Location = new System.Drawing.Point(251, 218);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(200, 173);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p4.TabIndex = 6;
+            this.p4.TabStop = false;
+            // 
+            // p3
+            // 
+            this.p3.Image = global::Galeria_De_Imagenes.Properties.Resources.Signo;
+            this.p3.Location = new System.Drawing.Point(19, 218);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(200, 173);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p3.TabIndex = 5;
+            this.p3.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.Image = global::Galeria_De_Imagenes.Properties.Resources.Signo;
+            this.p2.Location = new System.Drawing.Point(251, 27);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(200, 173);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p2.TabIndex = 4;
+            this.p2.TabStop = false;
+            // 
+            // p1
+            // 
+            this.p1.Image = global::Galeria_De_Imagenes.Properties.Resources.Signo;
+            this.p1.Location = new System.Drawing.Point(19, 27);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(200, 173);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p1.TabIndex = 3;
+            this.p1.TabStop = false;
+            this.p1.Click += new System.EventHandler(this.p1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.SteelBlue;
+            this.groupBox2.Controls.Add(this.pictureBox4);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 74);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(233, 597);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Galeria_De_Imagenes.Properties.Resources.Black_Clover;
+            this.pictureBox4.Location = new System.Drawing.Point(17, 410);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(200, 173);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.pictureBox4.MouseEnter += new System.EventHandler(this.pictureBox4_MouseEnter);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Galeria_De_Imagenes.Properties.Resources.Naruto;
+            this.pictureBox2.Location = new System.Drawing.Point(17, 218);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 173);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Galeria_De_Imagenes.Properties.Resources.One_Piece;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 173);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(289, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Personajes:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(652, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(800, 675);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Galeria De Imagenes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox p6;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Galeria_De_Imagenes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            p1.Image = Properties.Resources.o1;
+            p2.Image = Properties.Resources.o2;
+            p3.Image = Properties.Resources.o3;
+            p4.Image = Properties.Resources.o4;
+            p5.Image = Properties.Resources.o5;
+            p6.Image = Properties.Resources.o6;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            p1.Image = Properties.Resources.Signo;
+            p2.Image = Properties.Resources.Signo;
+            p3.Image = Properties.Resources.Signo;
+            p4.Image = Properties.Resources.Signo;
+            p5.Image = Properties.Resources.Signo;
+            p6.Image = Properties.Resources.Signo;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p1.Image = Properties.Resources.Signo;
+            p2.Image = Properties.Resources.Signo;
+            p3.Image = Properties.Resources.Signo;
+            p4.Image = Properties.Resources.Signo;
+            p5.Image = Properties.Resources.Signo;
+            p6.Image = Properties.Resources.Signo;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox4_MouseEnter(object sender, EventArgs e)
+        {
+            p1.Image = Properties.Resources.b1;
+            p2.Image = Properties.Resources.b2;
+            p3.Image = Properties.Resources.b3;
+            p4.Image = Properties.Resources.b4;
+            p5.Image = Properties.Resources.b5;
+            p6.Image = Properties.Resources.b6;
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            p1.Image = Properties.Resources.n1;
+            p2.Image = Properties.Resources.n2;
+            p3.Image = Properties.Resources.n3;
+            p4.Image = Properties.Resources.n4;
+            p5.Image = Properties.Resources.n5;
+            p6.Image = Properties.Resources.n6;
+        }
+
+        private void p1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
